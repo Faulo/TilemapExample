0106d72c185af4ecef0fa3698baaa17daede1199
Daniel Schulz <faulolio@gmail.com>
2020-12-01 15:41:46 +0100

    Create empty project

---
+++ Assets/Scripts/TilemapController.cs
@@ -0,0 +1,26 @@
+﻿using Slothsoft.UnityExtensions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapController : MonoBehaviour {
+    [SerializeField, Expandable]
+    Tilemap tilemap = default;
+    [SerializeField, Expandable]
+    TileBase borderTile = default;
+    [SerializeField, Range(1, 100)]
+    int width = 10;
+    [SerializeField, Range(1, 100)]
+    int height = 10;
+    [SerializeField]
+    bool createBorder = false;
+
+    void OnValidate() {
+        if (!tilemap) {
+            TryGetComponent(out tilemap);
+        }
+        if (createBorder) {
+            createBorder = false;
+            //tilemap.SetTiles();
+        }
+    }
+}
